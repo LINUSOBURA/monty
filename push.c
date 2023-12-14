1453337e88c81743d0f81b7d3c7b6876e7187cea
LINUSOBURA <linusobura@gmail.com>
2023-12-14 17:38:49 +0300

    initial commit

---
+++ push.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * push - function to add an item on top of the stack
+ * @stack: doubly linked list
+ * @value: the value to be stacked
+*/
+void push(stack_t **stack, int value)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+	(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
+
+/**
+ * pall - function to print all the values of the stack:
+ * @stack: doubly linked list
+ * @line_number: line number
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)line_number;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
